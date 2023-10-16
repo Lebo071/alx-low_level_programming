63142b52d7fa8eb99901eaf27c812b9fc6b21b42
root <lebomabokane@gmail.com>
2023-10-16 11:26:53 -0700

    0x05-pointersa-arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 
 	i = 0;
 	d = 0;
-	n = 0;
+
 	len = 0;
 	f = 0;
 	digit = 0;
@@ -25,25 +25,25 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
 
-	if (f == 0)
-		return (0);
 
-	return (n);
-}
+
+
+
+
+
+
+			
+
+		
+
+	
+
+
+
+
+
+
 
 
 
